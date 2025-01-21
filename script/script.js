dce2a9b9568900bff3c868d2fc0a2e7b39a66e87
moebamba01 <djigal001@gmail.com>
2025-01-21 12:16:48 +0000

    adding oop

--- script/script.js
+++ script/script.js
@@ -1,58 +1,127 @@
-// --------------------get all the buttons--------------------------------
-var plusButton = document.querySelectorAll('.btnPlus');
-let deleteButton = document.querySelectorAll('.btn-outline-danger');
-let heartIcons = document.querySelectorAll('.fa-heart');
-let article = document.querySelectorAll('.article')
-let qty = document.querySelectorAll('.quantite')
-let total = document.querySelectorAll('.total')
-let price = document.querySelectorAll('.prix')
-let mns = document.querySelectorAll('.btn-outline-warning')
-let sum = document.querySelector('.Prixtotal')
-// -----------------calculate the total price of the products---------------
-function calculateTotal() {
-  let totalPrice = 0;
-  for (let i = 0; i < article.length; i++) {
-    let itemPrice = parseInt(price[i].innerHTML);
-    let itemQty = parseInt(qty[i].value);
-    let itemTotal = itemPrice * itemQty;
-    total[i].innerHTML = itemTotal;
-    totalPrice += itemTotal;
-  }
-  sum.value = totalPrice;
-  console.log(article);
-}
-// ----------------add a click event listener to each plus button----------
-for (let i = 0; i < plusButton.length; i++) { 
-  plusButton[i].addEventListener('click', function(){
-    qty[i].value ++ ;
-    calculateTotal();
-  });
-  // -------------add a click event listener to each minus button-------------
-  mns[i].addEventListener('click' , function(){
-    if(qty[i].value > 0){
-      qty[i].value --;
-      calculateTotal();
-    }
-  })
-  // -------------add a click event listener to each delete button------------
-  deleteButton[i].addEventListener('click' , function(){
-    article[i].remove();
-    let articlePrice = parseInt(price[i].innerHTML) * parseInt(qty[i].innerHTML);
-    let currentTotal = parseInt(sum[0].value);
-    sum[0].value = currentTotal - articlePrice;
-    calculateTotal()
-  })
-}
-// -----------add a click event listener to each heart icon------------
-heartIcons.forEach((icon) => {
-  icon.addEventListener('click', () => {
-    // toggle the 'liked' class on the clicked icon
-    icon.classList.toggle('liked');
-  });
-});
-// -------------------calculate the initial total price on page load------------------
-calculateTotal(); 
+class ShoppingCart {
+  constructor() {
+    this.plusButton = document.querySelectorAll('.btnPlus');
+    this.deleteButton = document.querySelectorAll('.btn-outline-danger');
+    this.heartIcons = document.querySelectorAll('.fa-heart');
+    this.article = document.querySelectorAll('.article');
+    this.qty = document.querySelectorAll('.quantite');
+    this.total = document.querySelectorAll('.total');
+    this.price = document.querySelectorAll('.prix');
+    this.mns = document.querySelectorAll('.btn-outline-warning');
+    this.sum = document.querySelector('.Prixtotal');
+    this.init();
 
+   
+
+  }
+
+  calculateTotal() {
+    let totalPrice = 0;
+    for (let i = 0; i < this.article.length; i++) {
+      let itemPrice = parseInt(this.price[i].innerHTML);
+      let itemQty = parseInt(this.qty[i].value);
+      let itemTotal = itemPrice * itemQty;
+      this.total[i].innerHTML = itemTotal;
+      totalPrice += itemTotal;
+    }
+    this.sum.value = totalPrice;
+  }
+
+  init() {
+    for (let i = 0; i < this.plusButton.length; i++) {
+      this.plusButton[i].addEventListener('click', () => {
+        this.qty[i].value++;
+        this.calculateTotal();
+      });
+
+      this.mns[i].addEventListener('click', () => {
+        if (this.qty[i].value > 0) {
+          this.qty[i].value--;
+          this.calculateTotal();
+        }
+      });
+
+      this.deleteButton[i].addEventListener('click', () => {
+        this.article[i].remove();
+        let articlePrice = parseInt(this.price[i].innerHTML) * parseInt(this.qty[i].innerHTML);
+        let currentTotal = parseInt(this.sum.value);
+        this.sum.value = currentTotal - articlePrice;
+        this.calculateTotal();
+      });
+    }
+
+    this.heartIcons.forEach((icon) => {
+      icon.addEventListener('click', () => {
+        icon.classList.toggle('liked');
+      });
+    });
+
+    this.calculateTotal();
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  new ShoppingCart();
+});
+
+        let articlePrice = parseInt(this.price[i].innerHTML) * parseInt(this.qty[i].value);
+
+
+
+// // --------------------get all the buttons--------------------------------
+// var plusButton = document.querySelectorAll('.btnPlus');
+// let deleteButton = document.querySelectorAll('.btn-outline-danger');
+// let heartIcons = document.querySelectorAll('.fa-heart');
+// let article = document.querySelectorAll('.article')
+// let qty = document.querySelectorAll('.quantite')
+// let total = document.querySelectorAll('.total')
+// let price = document.querySelectorAll('.prix')
+// let mns = document.querySelectorAll('.btn-outline-warning')
+// let sum = document.querySelector('.Prixtotal')
+// // -----------------calculate the total price of the products---------------
+// function calculateTotal() {
+//   let totalPrice = 0;
+//   for (let i = 0; i < article.length; i++) {
+//     let itemPrice = parseInt(price[i].innerHTML);
+//     let itemQty = parseInt(qty[i].value);
+//     let itemTotal = itemPrice * itemQty;
+//     total[i].innerHTML = itemTotal;
+//     totalPrice += itemTotal;
+//   }
+//   sum.value = totalPrice;
+//   console.log(article);
+// }
+// // ----------------add a click event listener to each plus button----------
+// for (let i = 0; i < plusButton.length; i++) { 
+//   plusButton[i].addEventListener('click', function(){
+//     qty[i].value ++ ;
+//     calculateTotal();
+//   });
+//   // -------------add a click event listener to each minus button-------------
+//   mns[i].addEventListener('click' , function(){
+//     if(qty[i].value > 1){
+//       qty[i].value --;
+//       calculateTotal();
+//     }
+//   })
+//   // -------------add a click event listener to each delete button------------
+//   deleteButton[i].addEventListener('click' , function(){
+//     article[i].remove();
+//     let articlePrice = parseInt(price[i].innerHTML) * parseInt(qty[i].innerHTML);
+//     let currentTotal = parseInt(sum[0].value);
+//     sum[0].value = currentTotal - articlePrice;
+//     calculateTotal()
+//   })
+// }
+// // -----------add a click event listener to each heart icon------------
+// heartIcons.forEach((icon) => {
+//   icon.addEventListener('click', () => {
+//     // toggle the 'liked' class on the clicked icon
+//     icon.classList.toggle('liked');
+//   });
+// });
+// // -------------------calculate the initial total price on page load------------------
+// calculateTotal(); 
 
 
 
